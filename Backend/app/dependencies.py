cb1891a370eb937a4714ff798c6c0043236e55fe
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-07 17:25:55 +0200

    feat(auth): add get_user dependency

--- Backend/app/dependencies.py
+++ Backend/app/dependencies.py
@@ -1,8 +1,28 @@
 from typing import AsyncGenerator
 
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jose import ExpiredSignatureError, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.auth import User, verify_access_token
 from app.db import AsyncSessionLocal
+from app.exceptions import UnauthorizedException
+
+security = HTTPBearer()
+
+
+async def get_user(
+    auth_header: HTTPAuthorizationCredentials = Depends(security),
+) -> User:
+    token = auth_header.credentials
+    try:
+        user = await verify_access_token(token)
+        return user
+    except ExpiredSignatureError:
+        raise UnauthorizedException("Session expired")
+    except (JWTError, ValueError):
+        raise UnauthorizedException("Invalid token")
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
