b75d7dbe5e16ccdc521a4e23caf43ebdf7efd919
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-07 02:45:35 +0200

    chore: initial project structure

---
+++ Backend/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from app.config import settings
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="app.app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=settings.ENVIRONMENT == "DEV",
+    )
