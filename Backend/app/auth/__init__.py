c48f0a5d467cc9b5a9d6a6a0818bbed1b20b56ac
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-07 16:52:03 +0200

    feat(auth): add JWT verfication and User dto

---
+++ Backend/app/auth/__init__.py
@@ -0,0 +1,2 @@
+from .jwt import verify_access_token  # noqa: F401
+from .user_dto import User  # noqa: F401
---
+++ Backend/app/auth/jwt.py
@@ -0,0 +1,39 @@
+import uuid
+from typing import Optional, TypedDict, cast
+
+from jose import jwt
+
+from app.auth.cache import jwks_cache
+from app.auth.user_dto import User
+from app.config import settings
+
+
+class JWTPayload(TypedDict):
+    sub: str
+
+
+async def verify_access_token(token: str) -> User:
+    kid = cast(Optional[str], jwt.get_unverified_header(token).get("kid", None))
+
+    if kid is None:
+        raise ValueError("missing kid")
+
+    public_key = await jwks_cache.get_public_key(kid)
+
+    payload = cast(
+        JWTPayload,
+        jwt.decode(
+            token=token,
+            key=public_key,
+            algorithms=["ES256"],
+            issuer=settings.SUPABASE_EXPECTED_ISSUER,
+            audience=settings.SUPABASE_EXPECTED_AUDIENCE,
+        ),
+    )
+
+    user_id = uuid.UUID(payload["sub"])
+
+    if not user_id:
+        raise ValueError("missing sub")
+
+    return User(user_id)
