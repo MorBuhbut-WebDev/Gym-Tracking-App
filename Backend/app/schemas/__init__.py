939716a452369ba9e2b5ee1c15fe387cf103ea06
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-09 13:53:49 +0200

    feat(schemas): add routines exercises related schemas

--- Backend/app/schemas/__init__.py
+++ Backend/app/schemas/__init__.py
@@ -1,2 +1,7 @@
 from .exercises import ExerciseCreate, ExerciseResponse, ExerciseUpdate  # noqa: F401
 from .routines import RoutineCreate, RoutineUpdate, RoutineResponse  # noqa: F401
+from .routines_exercises import (
+    RoutineAddExercise,  # noqa: F401
+    RoutineUpdateExercise,  # noqa: F401
+    RoutineExerciseResponse,  # noqa: F401
+)
---
+++ Backend/app/schemas/routines_exercises.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, model_validator, PositiveInt
+from typing import Optional, Self
+
+from app.schemas.types import NotEmptyString
+
+
+class RoutineAddExercise(BaseModel):
+    planned_sets: PositiveInt = 3
+    exercise_notes: Optional[NotEmptyString] = None
+
+
+class RoutineUpdateExercise(BaseModel):
+    planned_sets: Optional[PositiveInt] = None
+    exercise_notes: Optional[NotEmptyString] = None
+
+    @model_validator(mode="after")
+    def ensure_at_least_one_exist(self) -> Self:
+        if self.planned_sets is None and self.exercise_notes is None:
+            raise ValueError("planned sets | exercise notes must be provided")
+        return self
+
+
+class RoutineExerciseResponse(BaseModel):
+    exercise_id: int
+    routine_id: int
+    exercise_index: int
+    planned_sets: int
+    exercise_notes: Optional[str]
+
+    model_config = {"from_attributes": True}
