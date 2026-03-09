feb2cce244516080085cc8afdf73982cd7f88a39
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-09 02:40:57 +0200

    feat(schemas): add routines related schemas

--- Backend/app/schemas/__init__.py
+++ Backend/app/schemas/__init__.py
@@ -1,1 +1,2 @@
 from .exercises import ExerciseCreate, ExerciseResponse, ExerciseUpdate  # noqa: F401
+from .routines import RoutineCreate, RoutineUpdate, RoutineResponse  # noqa: F401
---
+++ Backend/app/schemas/routines.py
@@ -0,0 +1,24 @@
+import uuid
+from pydantic import BaseModel
+
+from app.schemas.types import Name
+
+
+class RoutineBase(BaseModel):
+    routine_name: Name
+
+
+class RoutineCreate(RoutineBase):
+    pass
+
+
+class RoutineUpdate(RoutineBase):
+    pass
+
+
+class RoutineResponse(BaseModel):
+    routine_id: int
+    user_id: uuid.UUID
+    routine_name: str
+
+    model_config = {"from_attributes": True}
