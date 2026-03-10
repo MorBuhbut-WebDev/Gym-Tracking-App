9bc8b94926d9f501341455c8c058f0763b2e0522
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-10 03:13:45 +0200

    feat(db): add lazy workouts repositories initialization

--- Backend/app/repositories/__init__.py
+++ Backend/app/repositories/__init__.py
@@ -1,3 +1,6 @@
 from .exercises import ExerciseRepo  # noqa: F401
 from .routines import RoutineRepo  # noqa: F401
 from .routines_exercises import RoutineExerciseRepo  # noqa: F401
+from .workouts import WorkoutRepo  # noqa: F401
+from .workouts_exercises import WorkoutExerciseRepo  # noqa: F401
+from .workouts_sets import WorkoutSetRepo  # noqa: F401
