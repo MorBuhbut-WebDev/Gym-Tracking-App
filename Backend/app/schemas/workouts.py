750e65b153b6b258731858a000fa2da6dae501dc
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-10 03:39:03 +0200

    add WorkoutFilters schema

--- Backend/app/schemas/workouts.py
+++ Backend/app/schemas/workouts.py
@@ -1,12 +1,46 @@
-from pydantic import BaseModel, PositiveInt
-from typing import Optional
-from datetime import datetime
+from pydantic import BaseModel, PositiveInt, Field, model_validator
+from typing import Optional, Self
+from datetime import datetime, timedelta, date, timezone
 
 from app.schemas.workouts_exercises import WorkoutExerciseNested
 
 
 class WorkoutCreate(BaseModel):
     routine_id: PositiveInt
+
+
+class WorkoutFilters(BaseModel):
+    start_date: date = Field(default_factory=lambda: date.today().replace(day=1))
+    end_date: Optional[date] = Field(default=None)
+
+    @model_validator(mode="after")
+    def set_end_date_and_convert(self) -> Self:
+        if self.end_date is None:
+            if self.start_date.month == 12:
+                self.end_date = date(self.start_date.year + 1, 1, 1)
+            else:
+                self.end_date = date(self.start_date.year, self.start_date.month + 1, 1)
+        else:
+            if self.end_date < self.start_date:
+                raise ValueError("end date must be greater or equal to the start date")
+
+            self.end_date = self.end_date + timedelta(days=1)
+        return self
+
+    def to_datetime(self) -> tuple[datetime, datetime]:
+        start = datetime(
+            self.start_date.year,
+            self.start_date.month,
+            self.start_date.day,
+            tzinfo=timezone.utc,
+        )
+        end = datetime(
+            self.end_date.year,
+            self.end_date.month,
+            self.end_date.day,
+            tzinfo=timezone.utc,
+        )
+        return start, end
 
 
 class WorkoutBase(BaseModel):
