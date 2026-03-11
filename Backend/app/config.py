bb0e18f0ca5059c987f4086fea90ca0e8b75f27a
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-11 02:05:17 +0200

    chore: set up fast api and env configuration

---
+++ Backend/app/config.py
@@ -0,0 +1,33 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    SUPABASE_URL: str
+    SUPABASE_EXPECTED_AUDIENCE: str = "authenticated"
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    @property
+    def SUPABASE_EXPECTED_ISSUER(self) -> str:
+        return f"{self.SUPABASE_URL}/auth/v1"
+
+    @property
+    def SUPABASE_JWKS_URl(self) -> str:
+        return f"{self.SUPABASE_URL}/auth/v1/.well-known/jwks.json"
+
+    @property
+    def database_url_async(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def database_url_sync(self) -> str:
+        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+settings = Settings()  # type: ignore
