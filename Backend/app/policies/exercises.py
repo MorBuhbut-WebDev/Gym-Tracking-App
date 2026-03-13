b092f50b6dc847eea3b95a0854b2e51579412868
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-13 17:42:30 +0200

    feat(policies): add ExercisePolicy to enforce exercise business rules

---
+++ Backend/app/policies/exercises.py
@@ -0,0 +1,30 @@
+import uuid
+
+from app.exceptions import ConflictException, NotFoundException
+from app.models import Exercise
+from app.repositories import ExerciseRepo
+
+
+class ExercisePolicy:
+    @staticmethod
+    async def assert_exists(
+        repo: ExerciseRepo, user_id: uuid.UUID, exercise_id: int
+    ) -> Exercise:
+        exercise = await repo.get_by_id(exercise_id, user_id)
+
+        if exercise is None:
+            raise NotFoundException(f"Exercise with id: {exercise_id} is not found")
+
+        return exercise
+
+    @staticmethod
+    async def assert_name_is_unique(
+        repo: ExerciseRepo,
+        user_id: uuid.UUID,
+        exercise_name: str,
+        exercise_id: int | None = None,
+    ) -> None:
+        exercise = await repo.get_by_name(exercise_name, exercise_id, user_id)
+
+        if exercise is not None:
+            raise ConflictException(f"{exercise_name} already exists")
