84b0dc53762c47712cb72bcbc09d9fc5369e16cf
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-13 16:53:28 +0200

    feat(repositories): add BaseRepo for basic CRUD operations

---
+++ Backend/app/repositories/base.py
@@ -0,0 +1,40 @@
+from typing import TypeVar, cast
+
+from pydantic import BaseModel
+from sqlalchemy import BinaryExpression, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import Base
+
+Model = TypeVar("Model", bound=Base)
+
+
+class BaseRepo[Model]:
+    def __init__(self, model: type[Model], session: AsyncSession) -> None:
+        self._model = model
+        self._session = session
+
+    async def _execute_query(self, condition: BinaryExpression):
+        query = select(self._model).where(condition)
+        return await self._session.execute(query)
+
+    def add(self, obj: Model) -> Model:
+        self._session.add(obj)
+        return obj
+
+    async def get_all(self, condition: BinaryExpression) -> list[Model]:
+        result = await self._execute_query(condition)
+        return cast(list[Model], result.scalars().all())
+
+    async def get(self, condition: BinaryExpression) -> Model | None:
+        result = await self._execute_query(condition)
+        return result.scalar_one_or_none()
+
+    def update(self, old: Model, updated: BaseModel) -> Model:
+        for col, value in updated.model_dump(exclude_unset=True).items():
+            if hasattr(old, col):
+                setattr(old, col, value)
+        return old
+
+    async def delete(self, obj: Model) -> None:
+        await self._session.delete(obj)
