3755fd82e18a3799435cfa0e9a3460f2b978680e
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-15 01:36:50 +0200

    feat(routes): add routines exercises router

--- Backend/app/routes/routines.py
+++ Backend/app/routes/routines.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter, Depends
 from app.auth import User
 from app.db import UnitOfWork
 from app.dependencies import get_uow, get_user
+from app.routes.routines_exercises import routine_exercise_router
 from app.schemas import RoutineCreate, RoutineResponse, RoutineUpdate
 from app.services import RoutineService, get_routines_service
 
 routines_router = APIRouter(prefix="/routines")
+routines_router.include_router(routine_exercise_router)
 
 
 @routines_router.post("/", response_model=RoutineResponse, status_code=201)
