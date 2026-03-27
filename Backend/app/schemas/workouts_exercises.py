9ff46d6ba9a079c989a781e431d5d9be8af91843
Mor Buhbut <mbuhhbut2005@gmail.com>
2026-03-27 04:09:52 +0300

    refactor(schemas): remove unnecessary workouts exercises schemas

--- Backend/app/schemas/workouts_exercises.py
+++ Backend/app/schemas/workouts_exercises.py
@@ -1,18 +1,9 @@
-from pydantic import BaseModel
-
 from app.schemas.shared import AppBaseModel
 from app.schemas.workouts_sets import WorkoutSetResponse
 
 
-class WorkoutExerciseBase(BaseModel):
-    exercise_index: int
-
-
-class WorkoutExerciseNested(WorkoutExerciseBase, AppBaseModel):
+class WorkoutExerciseNested(AppBaseModel):
     exercise_id: int
+    exercise_index: int
     exercise_name: str
     sets: list[WorkoutSetResponse]
-
-
-class WorkoutExerciseResponse(WorkoutExerciseBase, AppBaseModel):
-    pass
